24f7baa61fcf2cd033f3a61acd4e9f4bdaf69560
poldevy <rostyslavshyian@gmail.com>
2024-05-06 14:28:29 +0200

    finished 53 post

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 // app.get("/", (req, res) => {
 //   //   res.status(200).send("Hello from the server side...");
 //   res
@@ -23,6 +25,26 @@ app.get("/api/v1/tours", (req, res) => {
     results: tours.length,
     data: { tours: tours },
   });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const tour = { ...req.body, id: newId };
+  const updatedTours = [...tours, tour];
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(updatedTours),
+    (err) => {
+      res.status(201).json({
+        status: " success",
+        data: {
+          tour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
