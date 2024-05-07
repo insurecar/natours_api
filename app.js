db092999579685458c3060744e496686ed92a3cb
poldevy <rostyslavshyian@gmail.com>
2024-05-07 12:06:51 +0200

    added gdusers

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const morgan = require("morgan");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const app = express();
+const fs = require("fs");
+
+const getAllGDUsers = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/gdUsers.json`)
+);
 
 // 1_ Middleware
 app.use(morgan("dev"));
@@ -42,6 +47,33 @@ app.use((req, res, next) => {
 // app.patch("/api/v1/tours/:id", updateTour);
 
 // app.delete("/api/v1/tours/:id", deleteTour);
+
+app.get("/api/v1/gd-users", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: getAllGDUsers.length,
+    data: {
+      gdusers: getAllGDUsers,
+    },
+  });
+});
+
+app.get("/api/v1/gd-users/:lastname", (req, res) => {
+  console.log(req.params);
+  const findUsers = getAllGDUsers.filter(
+    (user) =>
+      // user?.lastName.toLowerCase() === req.params?.lastname.toLowerCase() ||
+      user?.firstName.toLowerCase() === req.params?.lastname.toLowerCase()
+  );
+  res.status(200).json({
+    status: "success",
+    results: findUsers.length,
+    data: {
+      users: findUsers,
+    },
+  });
+});
+
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
