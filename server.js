27fe4cce6c648150a8be054af685c2514523e148
poldevy <rostyslavshyian@gmail.com>
2024-05-24 14:11:25 +0200

    added schema

--- server.js
+++ server.js
@@ -1,32 +1,17 @@
 const mongoose = require("mongoose");
 
-const { MongoClient } = require("mongodb");
-
 const dotenv = require("dotenv");
 
 dotenv.config({
   path: "config.env",
 });
+const app = require("./app");
+
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
-// const client = new MongoClient(DB, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
 
-// async function connectToDatabase() {
-//   try {
-//     await client.connect();
-//     console.log("Connected to the database");
-//   } catch (error) {
-//     console.error("Error connecting to the database:", error);
-//   } finally {
-//     await client.close();
-//   }
-// }
-// connectToDatabase();
 main().catch((err) => console.log(err));
 
 async function main() {
@@ -34,17 +19,13 @@ async function main() {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true,
-    useUnifiedTopology: true,
   });
 }
-console.log(process.env.DATABASE_PASSWORD);
-
-const app = require("./app");
 
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "A tour must have a name"],
+    required: [true, "😞 A tour must have a name"],
     unique: true,
   },
   raiting: {
@@ -57,7 +38,21 @@ const tourSchema = new mongoose.Schema({
   },
 });
 
+const userSchema = new mongoose.Schema({
+  name: String,
+  surname: String,
+});
+const User = mongoose.model("User", userSchema); // "User - this entity will be created in mongodb"
 const Tour = mongoose.model("Tour", tourSchema);
+
+const testTour = new Tour({
+  rating: 123,
+});
+
+testTour
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((err) => console.log("💥💥💥💥", err));
 
 const port = process.env.PORT || 3000;
 
