800067d3107dfd4611332b3c326ff69cd0317276
poldevy <rostyslavshyian@gmail.com>
2024-06-17 17:06:39 +0200

    finished 95 query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,3 @@
-const fs = require("fs");
 const Tour = require("../models/tourModel");
 
 // const tours = JSON.parse(
@@ -31,7 +30,18 @@ const Tour = require("../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const tours = await Tour.find(queryObj);
+
+    // const tours = await Tour.find()
+    //   .where("duration")
+    //   .equals(5)
+    //   .where("difficulty")
+    //   .equals("easy");
+
     res.status(200).json({
       status: "success",
       requstedAt: req.requestTime,
