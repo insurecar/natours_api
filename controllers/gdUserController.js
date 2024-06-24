fc935c0cad3a3e66a2a5465a26a370c5fd47b86a
poldevy <rostyslavshyian@gmail.com>
2024-06-24 14:43:15 +0200

    added filter for gdusers

--- controllers/gdUserController.js
+++ controllers/gdUserController.js
@@ -1,42 +1,15 @@
 const GDUser = require("../models/gdModel");
+const APIFeatures = require("../utils/apiFeatures");
 
 exports.getAllUsers = async (req, res) => {
   try {
-    const queryObj = { ...req.query };
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((query) => delete queryObj[query]);
+    const features = new APIFeatures(GDUser.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = JSON.parse(
-      queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
-    );
-
-    let query = GDUser.find(queryStr);
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort("id");
-    }
-
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    } else {
-      query = query.select("-__v"); // excluded property
-    }
-
-    const page = +req.query.page || 1;
-    const limit = +req.query.limit || 10000;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await GDUser.countDocuments();
-      if (skip >= numTours) throw new Error(`This page doesn't exist`);
-    }
-
-    const gdusers = await query;
+    const gdusers = await features.query;
     res.status(200).json({
       status: "success",
       length: gdusers.length,
