1dc185a98456feadae892af78883b2f284b55c3c
poldevy <rostyslavshyian@gmail.com>
2024-07-08 17:05:53 +0200

    finished 114

--- app.js
+++ app.js
@@ -23,9 +23,24 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/gdusers", gdUserRoute);
 
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl}`,
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.originalUrl}`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl}`);
+  err.status = "fail";
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
